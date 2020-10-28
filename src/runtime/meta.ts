60e6f990cf6a4db8537bc8f55b8a1b71b2eeb6f5
Pooya Parsa <pyapar@gmail.com>
2020-10-28 17:58:02 +0100

    fix import

--- src/runtime/meta.ts
+++ src/runtime/meta.ts
@@ -25,7 +25,7 @@ export async function getMeta (url) {
   }
 
   if (process.server) {
-    const imageMeta = await import('image-meta')
+    const imageMeta = require('image-meta')
     const data: Buffer = await fetch(url).then((res: any) => res.buffer())
     const { width, height, mimeType } = await imageMeta(data)
     return {
