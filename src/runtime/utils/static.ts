1975337435f89e477fab435148791dedac1377bc
Farnabaz <farnabaz@gmail.com>
2021-01-26 20:37:03 +0330

    chore: refactor

--- src/runtime/utils/static.ts
+++ src/runtime/utils/static.ts
@@ -1,20 +1,23 @@
 const manifest = {}
 
+function updateManifest () {
+  if (typeof window.$nuxt !== 'undefined') {
+    const pageImages = (window.$nuxt as any)._pagePayload?.data?.[0]?._img || {}
+    Object.assign(manifest, pageImages)
+  }
+}
+
 export function useManifest (nuxtContext?) {
   if (process.client) {
-    if (typeof window.$nuxt !== 'undefined') {
-      const pageImages = (window.$nuxt as any)._pagePayload?.data?.[0]?._img || {}
-      Object.assign(manifest, pageImages)
-      return manifest
-    }
+    updateManifest()
 
     // merge new manifests on route change
     if (nuxtContext) {
-      nuxtContext.app.router.afterEach(() => useManifest())
+      nuxtContext.app.router.afterEach(updateManifest)
     }
 
     // make sure manifest is initialized
-    (window as any).onNuxtReady(useManifest)
+    (window as any).onNuxtReady(updateManifest)
   }
   return manifest
 }
