af0698d152b7a8c33315b4c3ec6f5cc22780df02
Pooya Parsa <pyapar@gmail.com>
2021-02-15 11:07:06 +0100

    update responsive playground

--- playground/pages/responsive.vue
+++ playground/pages/responsive.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <h2>nuxt-img</h2>
+    <!-- <h2>nuxt-img</h2>
     <NuxtImg
       :sizes="{ sm: 100, md: 500 }"
       src="/images/damavand.jpg"
@@ -10,6 +10,53 @@
     <NuxtPicture
       :sizes="{ sm: 100, md: 500 }"
       src="/images/damavand.jpg"
+    /> -->
+    <div style="width: 50vw">
+      i'm 50vw
+    </div>
+    <nuxt-img
+      src="/logos/nuxt.png"
+      sizes="lg:400 sm:100vw md:50vw"
     />
+    <nuxt-img
+      src="/logos/nuxt.png"
+      :sizes="{ sm: '100vw', md: '50vw', default: 400 }"
+    />
+    <img
+      src="/logos/nuxt.png"
+      sizes="(max-width: 640px) 100vw, (max-width: 768px) 50vw, 400px"
+      :srcset="`${l(320)} 320w,${l(640)} 640w,${l(384)} 384w,${l(400)} 400w`"
+    >
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    l (txt) {
+      return 'https://textoverimage.moesif.com/image?image_url=https%3A%2F%2Fraw.githubusercontent.com%2Fnuxt%2Fnuxt.js%2Fdev%2F.github%2Fnuxt.png&text=' + txt
+    }
+  }
+}
+</script>
+
+<style scoped>
+@media (max-width: 640px) {
+  img2 {
+    max-width: 100vw;
+    width: 100vw;
+  }
+}
+
+@media (max-width: 768px) {
+  img2 {
+    max-width: 50vw;
+    width: 50vw;
+  }
+}
+
+img2 {
+  max-width: 300px;
+  width: 300px;
+}
+</style>
