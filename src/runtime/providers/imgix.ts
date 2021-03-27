3d4eac59e16f9e241c99646a679394802b2692d7
danny-pearson <danny@crtr.co.uk>
2021-03-27 19:04:29 +0000

    feat: Adds logic to enable secure URLs in the imgix provider

--- src/runtime/providers/imgix.ts
+++ src/runtime/providers/imgix.ts
@@ -1,4 +1,5 @@
 import type { ProviderGetImage } from 'src'
+import md5 from 'md5'
 import { joinURL } from 'ufo'
 import { createOperationsGenerator } from '~image'
 
@@ -18,12 +19,39 @@ const operationsGenerator = createOperationsGenerator({
     }
   },
   joinWith: '&',
-  formatter: (key, value) => `${key}=${value}`
+  formatter: (key, value) => {
+    const encodedKey = encodeURIComponent(key)
+    const encodedValue = encodeURIComponent(value)
+
+    return `${encodedKey}=${encodedValue}`
+  }
 })
 
-export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL = '/' } = {}) => {
+const sanitizePath = (src: string): string => {
+  const path = src.replace(/^\//, '')
+
+  const encodedPath = /^https?:\/\//.test(path)
+    ? encodeURIComponent(path)
+    : encodeURI(path)
+      .replace(/[#?:+]/g, encodeURIComponent)
+
+  return `/${encodedPath}`
+}
+
+export const getImage: ProviderGetImage = (src, { options = {}, modifiers = {}, baseURL = '/' } = {}) => {
+  const { token } = options
   const operations = operationsGenerator(modifiers)
+  const queryString = operations ? `?${operations}` : ''
+
+  if (!token) {
+    return { url: joinURL(baseURL, src + queryString) }
+  }
+
+  const sanitizedPath = sanitizePath(src)
+  const signature = md5(token + sanitizedPath + queryString)
+  const delimiter = queryString ? '&' : '?'
+
   return {
-    url: joinURL(baseURL, src + (operations ? ('?' + operations) : ''))
+    url: joinURL(baseURL, sanitizedPath + queryString + delimiter + `s=${signature}`)
   }
 }
