342e78ae05161f34fcc1a49d70288379eb888f07
Pooya Parsa <pyapar@gmail.com>
2021-04-22 21:00:39 +0200

    chore: remove duplicate code from test-utils preset

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,8 @@
 module.exports = {
   preset: '@nuxt/test-utils',
   transform: {
-    '\\.(js|ts)$': [
-      'babel-jest',
-      {
-        presets: [
-          ['@babel/preset-env', { targets: { node: 'current' } }],
-          '@babel/preset-typescript'
-        ],
-        plugins: ['@babel/plugin-transform-runtime']
-      }
-    ],
     '^.+\\.vue$': 'vue-jest'
   },
-  moduleFileExtensions: ['js', 'ts', 'vue', 'json'],
   moduleNameMapper: {
     '~image/(.*)': '<rootDir>/src/runtime/$1',
     '~image': '<rootDir>/src/runtime/index.ts',
