fc7278e4f983537706c52f11b312c8eed0024447
Daniel Roe <daniel@roe.dev>
2021-04-30 22:06:39 +0100

    feat: snap to larger width in dev/prod

--- src/runtime/providers/vercel.ts
+++ src/runtime/providers/vercel.ts
@@ -4,21 +4,32 @@ import { stringifyQuery } from 'ufo'
 // https://vercel.com/docs/more/adding-your-framework#images
 
 export const getImage: ProviderGetImage = (src, { modifiers, baseURL = '/_vercel/image' } = {}, ctx) => {
+  const validWidths = Object.values(ctx.options.screens || {}).sort()
+  const largestWidth = validWidths[validWidths.length - 1]
+  let width = Number(modifiers?.width || 0)
+
+  if (!width) {
+    width = largestWidth
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line
+      console.warn(`A defined width should be provided to use the \`vercel\` provider. Defaulting to \`${largestWidth}\`. Warning originated from \`${src}\`.`)
+    }
+  } else if (!validWidths.includes(width)) {
+    width = validWidths.find(validWidth => validWidth > width) || largestWidth
+    if (process.env.NODE_ENV === 'development') {
+      // eslint-disable-next-line
+      console.warn(`The width being used (\`${modifiers?.width}\`) should be added to \`image.screens\`. Defaulting to \`${width}\`. Warning originated from \`${src}\`.`)
+    }
+  }
+
   if (process.env.NODE_ENV === 'development') {
-    if (!modifiers?.width) {
-      console.warn(`A defined width must be provided to use the \`vercel\` provider. Warning originated from \`${src}\`.`)
-      return { url: '/_vercel/width-required' }
-    }
-    if (!Object.values(ctx.options.screens || {}).includes(Number(modifiers.width))) {
-      console.warn(`The width being used (\`${modifiers.width}\`) should be added to \`image.screens\`. Warning originated from \`${src}\`.`)
-      return { url: '/_vercel/invalid-width' }
-    }
     return { url: src }
   }
+
   return {
     url: baseURL + '?' + stringifyQuery({
       url: src,
-      w: modifiers?.width ? String(modifiers.width) : undefined,
+      w: String(width),
       q: String(modifiers?.quality || '100')
     })
   }
