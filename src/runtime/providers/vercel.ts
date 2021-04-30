349c1dea04c259b2b2814ec550bfacc3f76a51d3
Daniel Roe <daniel@roe.dev>
2021-04-30 16:09:24 +0100

    feat(vercel): add dev fallback provider for vercel

--- src/runtime/providers/vercel.ts
+++ src/runtime/providers/vercel.ts
@@ -3,7 +3,18 @@ import { stringifyQuery } from 'ufo'
 
 // https://vercel.com/docs/more/adding-your-framework#images
 
-export const getImage: ProviderGetImage = (src, { modifiers, baseURL = '/_vercel/image' } = {}) => {
+export const getImage: ProviderGetImage = (src, { modifiers, baseURL = '/_vercel/image' } = {}, ctx) => {
+  if (process.env.NODE_ENV === 'development') {
+    if (!modifiers?.width) {
+      console.warn(`A defined width must be provided to use the \`vercel\` provider. Warning originated from \`${src}\`.`)
+      return { url: '/_vercel/width-required' }
+    }
+    if (!Object.values(ctx.options.screens || {}).includes(Number(modifiers.width))) {
+      console.warn(`The width being used (\`${modifiers.width}\`) should be added to \`image.screens\`. Warning originated from \`${src}\`.`)
+      return { url: '/_vercel/invalid-width' }
+    }
+    return { url: src }
+  }
   return {
     url: baseURL + '?' + stringifyQuery({
       url: src,
