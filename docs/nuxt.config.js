624f6293bf9cbacfb17ad0b7ad53ae72bf491c41
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-18 14:52:05 +0200

    :wrench: (nuxt) update nuxt config

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -1,9 +1,25 @@
+import { resolve } from 'path'
 import { withDocus } from 'docus'
 
 export default withDocus({
-  rootDir: __dirname,
+  rootDir: resolve(__dirname),
+  windicss: {
+    root: resolve(__dirname)
+  },
+  socialImage: {
+    baseUrl: 'DOMIN',
+    chrome: {
+      browserWSEndpoint: 'wss://chrome.browserless.io/'
+    }
+  },
   buildModules: ['vue-plausible'],
   plausible: {
     domain: 'image.nuxtjs.org'
+  },
+  hooks: {
+    ready(ctx) {
+      // console.log(ctx)
+      console.log(ctx.options.windicss)
+    }
   }
 })
