26bb38b245dffe369121267d7fb65bf258992713
Yaël GUILLOUX <yael.guilloux@gmail.com>
2021-05-20 23:41:49 +0200

    :wrench: (nuxt) remove unnecessary boilerplate

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -2,12 +2,6 @@ import { withDocus } from 'docus'
 
 export default withDocus({
   rootDir: __dirname,
-  socialImage: {
-    baseUrl: 'https://image.nuxtjs.org',
-    chrome: {
-      browserWSEndpoint: 'wss://chrome.browserless.io/'
-    }
-  },
   buildModules: ['vue-plausible'],
   plausible: {
     domain: 'image.nuxtjs.org'
