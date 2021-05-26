b1b3464591d7d13fd590447fc2bd298108a8951b
Clément Ollivier <clement.o2p@gmail.com>
2021-05-26 18:17:42 +0200

    docs: WIP on homepage

---
+++ docs/windi.config.js
@@ -0,0 +1,18 @@
+export default {
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary-green': '#00DC82',
+      'primary-green-darkest': '#003C3C',
+      secondary: '#002E3B',
+      'secondary-light': '#064A5B',
+      'secondary-dark': '#01232D',
+      'secondary-darker': '#001D25',
+      'secondary-darkest': '#003543',
+      terciary: '#A8DDDB',
+      black: '#000',
+      white: '#fff'
+    }
+  }
+}
