1de2e9183477aa27755444860e69245171a8b835
Daniel Roe <daniel@roe.dev>
2021-05-29 10:44:08 +0100

    feat: add development warnings and handling for invalid smartcrop

--- src/runtime/providers/netlify.ts
+++ src/runtime/providers/netlify.ts
@@ -18,15 +18,33 @@ export const operationsGenerator = createOperationsGenerator({
   formatter: (key, value) => `${key}=${value}`
 })
 
+const isDev = process.env.NODE_ENV === 'development'
+
 // https://docs.netlify.com/large-media/transform-images/
 
 export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL = '/' } = {}) => {
   if (modifiers.height || modifiers.width) {
-    modifiers.fit = modifiers.fit || 'contain'
+    if (!(modifiers.height && modifiers.width)) {
+      // smartcrop is only supported with both height and width
+      modifiers.fit = 'contain'
+      if (isDev) {
+        // eslint-disable-next-line
+        console.warn(`Reverting to fit=contain as smart cropping is only supported by providing both height and width. Warning originated from \`${src}\`.`)
+      }
+    } else {
+      modifiers.fit = modifiers.fit || 'contain'
+    }
   }
   if (modifiers.format) {
     // Not currently supported
     delete modifiers.format
+    if (isDev) {
+      // eslint-disable-next-line
+      console.warn(`Providing format is not currently supported by the Netlify provider. Warning originated from \`${src}\`.`)
+    }
+  }
+  if (isDev) {
+    return { url: src }
   }
   const operations = operationsGenerator(modifiers)
   return {
