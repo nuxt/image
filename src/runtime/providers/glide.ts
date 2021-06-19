466691cdd6b15e761df010713e8e5cddb0b10d64
Damien Robinson <damien.robinson@pocketpasta.com>
2021-06-19 11:54:12 +1000

    fix(glide): add keys

--- src/runtime/providers/glide.ts
+++ src/runtime/providers/glide.ts
@@ -1,17 +1,37 @@
 // https://glide.thephpleague.com/2.0/api/quick-reference/
 
 import { ProviderGetImage } from 'src'
-import { joinURL, encodeQueryItem, encodePath, hasProtocol } from 'ufo'
+import { joinURL, encodeQueryItem, encodePath } from 'ufo'
 import { createOperationsGenerator } from '~image'
 
 const operationsGenerator = createOperationsGenerator({
   keyMap: {
-    format: 'fm',
-    fit: 'fit',
+    orientation: 'or',
+    flip: 'flip',
+    crop: 'crop',
     width: 'w',
     height: 'h',
+    fit: 'fit',
+    dpr: 'dpr',
+    bri: 'bri',
+    con: 'con',
+    gam: 'gam',
+    sharp: 'sharp',
+    blur: 'blur',
+    pixel: 'pixel',
+    filt: 'filt',
+    mark: 'mark',
+    markw: 'markw',
+    markh: 'markh',
+    markx: 'markx',
+    marky: 'marky',
+    markpad: 'markpad',
+    markpos: 'markpos',
+    markalpha: 'markalpha',
+    background: 'bg',
+    border: 'border',
     quality: 'q',
-    background: 'bg'
+    format: 'fm'
   },
   joinWith: '&',
   formatter: (key, val) => encodeQueryItem(key, val)
