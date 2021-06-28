644384cf3621b273515d157f0af595f1c2e68f08
Farnabaz <farnabaz@gmail.com>
2021-06-28 12:15:35 +0430

    fix(vercel): validate remote domains

--- src/runtime/providers/vercel.ts
+++ src/runtime/providers/vercel.ts
@@ -1,12 +1,21 @@
 import { ProviderGetImage } from 'src'
-import { stringifyQuery } from 'ufo'
+import { stringifyQuery, hasProtocol } from 'ufo'
 
 // https://vercel.com/docs/more/adding-your-framework#images
 
-export const getImage: ProviderGetImage = (src, { modifiers, baseURL = '/_vercel/image' } = {}, ctx) => {
-  const validWidths = Object.values(ctx.options.screens || {}).sort()
+export const getImage: ProviderGetImage = (src, { modifiers, baseURL = '/_vercel/image' } = {}, { options: { domains = [], screens = {} } }) => {
+  const validWidths = Object.values(screens).sort()
   const largestWidth = validWidths[validWidths.length - 1]
   let width = Number(modifiers?.width || 0)
+
+  // Serve original src if domain is invalid
+  if (hasProtocol(src)) {
+    if (!domains.find((d: string) => src.startsWith(d))) {
+      return {
+        url: src
+      }
+    }
+  }
 
   if (!width) {
     width = largestWidth
