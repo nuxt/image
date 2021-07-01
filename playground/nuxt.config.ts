3d11b8781bc01667b1b6394ac34f36c57d461f16
pooya parsa <pyapar@gmail.com>
2021-07-01 13:44:17 +0200

    feat: support alias (#348)

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -16,11 +16,15 @@ export default <NuxtConfig> {
   image: {
     domains: [
       'https://nuxtjs.org',
-      'https://unsplash.com',
+      'https://images.unsplash.com',
       'https://upload.wikimedia.org'
     ],
     screens: {
       750: 750
+    },
+    alias: {
+      unsplash: 'https://images.unsplash.com', // ipx
+      blog: '/remote/nuxt-org/blog' // cloudinary
     },
     twicpics: {
       baseURL: 'https://demo.twic.pics/'
