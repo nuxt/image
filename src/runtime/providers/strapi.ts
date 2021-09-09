19307581050cc8806c826689de004519a3917360
reliut-g <reliutg@gmail.com>
2021-09-09 11:39:23 +0200

    refactor: use url utils from ufo

--- src/runtime/providers/strapi.ts
+++ src/runtime/providers/strapi.ts
@@ -1,26 +1,23 @@
 import { ProviderGetImage } from 'src'
+import { withBase, joinURL } from 'ufo'
 
 // https://strapi.io/documentation/developer-docs/latest/development/plugins/upload.html#upload
 
 export const getImage: ProviderGetImage = (src, { modifiers, baseURL = 'http://localhost:1337/uploads' } = {}) => {
-  const breakpoint = modifiers?.breakpoint || ''
+  const breakpoint = modifiers?.breakpoint ?? ''
+
+  if (!breakpoint) {
+    return {
+      url: withBase(joinURL(src), baseURL)
+    }
+  }
 
   if (src.startsWith('/')) {
     src = src.slice(1)
   }
 
-  if (baseURL.endsWith('/')) {
-    baseURL = baseURL.replace(/\/$/, '')
-  }
-
-  if (!breakpoint) {
-    return {
-      url: `${baseURL}/${src}`
-    }
-  }
-
   return {
-    url: `${baseURL}/${breakpoint}_${src}`
+    url: withBase(joinURL(`${breakpoint}_${src}`), baseURL)
   }
 }
 
