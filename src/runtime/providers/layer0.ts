ba42ebeec6cb56a09c390886edd8d30a681be3b8
Pooya Parsa <pyapar@gmail.com>
2022-06-21 12:28:06 +0200

    support custom baseURL

--- src/runtime/providers/layer0.ts
+++ src/runtime/providers/layer0.ts
@@ -2,8 +2,6 @@ import { joinURL } from 'ufo'
 import type { ProviderGetImage } from 'src'
 import { createOperationsGenerator } from '~image'
 
-const endpoint = 'https://opt.moovweb.net'
-
 const operationsGenerator = createOperationsGenerator({
   keyMap: {
     height: 'height',
@@ -14,10 +12,13 @@ const operationsGenerator = createOperationsGenerator({
   formatter: (key, value) => String(value) === 'true' ? key : `${key}=${value}`
 })
 
-export const getImage: ProviderGetImage = (src, { modifiers = {} } = {}) => {
+export const getImage: ProviderGetImage = (src, {
+  modifiers = {},
+  baseURL = 'https://opt.moovweb.net'
+} = {}) => {
   const operations = operationsGenerator(modifiers)
 
   return {
-    url: joinURL(endpoint, '?img=' + src + (operations ? ('&' + operations) : ''))
+    url: joinURL(baseURL, '?img=' + src + (operations ? ('&' + operations) : ''))
   }
 }
