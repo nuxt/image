e5ca160f73913d029e5b936ad8b882375254aaed
Pooya Parsa <pooya@pi0.io>
2022-07-07 23:52:30 +0200

    fix: add missing `defineNuxtPlugin` import

--- src/runtime/plugin.ts
+++ src/runtime/plugin.ts
@@ -1,5 +1,6 @@
 import { createImage } from '#image'
 import { imageOptions } from '#build/image-options'
+import { defineNuxtPlugin } from '#imports'
 
 export default defineNuxtPlugin(() => {
   const img = createImage(imageOptions)
