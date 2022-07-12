4018a921a836c632cb9a0d4ae1bdee932a5cbe31
Sébastien Chopin <seb@nuxtjs.com>
2022-07-13 01:59:15 +0200

    docs: update

---
+++ docs/tokens.config.ts
@@ -0,0 +1,18 @@
+import { defineThemeTokens } from '@nuxt-themes/kit'
+
+export default defineThemeTokens({
+  colors: {
+    primary: {
+      50: '#d6ffee',
+      100: '#acffdd',
+      200: '#83ffcc',
+      300: '#30ffaa',
+      400: '#00dc82',
+      500: '#00bd6f',
+      600: '#009d5d',
+      700: '#007e4a',
+      800: '#005e38',
+      900: '#003f25'
+    }
+  }
+})
