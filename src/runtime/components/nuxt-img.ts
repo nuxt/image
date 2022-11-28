bc9ddc0caf4fe32b63f18118ad1b392c7160ddf9
Ross <9055337+chadsr@users.noreply.github.com>
2022-11-28 13:46:55 +0100

    fix(nuxt-img): access `prerender.env` only in server side (#661)
    * check if process is undefined before accessing prerender env
    
    * Update src/runtime/components/nuxt-img.ts
    
    Co-authored-by: pooya parsa <pyapar@gmail.com>

--- src/runtime/components/nuxt-img.ts
+++ src/runtime/components/nuxt-img.ts
@@ -86,7 +86,7 @@ export default defineComponent({
       })
     }
 
-    if (process.env.prerender) {
+    if (process.server && process.env.prerender) {
       const sources = [
         src.value,
         ...(sizes.value.srcset || '').split(',').map(s => s.split(' ')[0])
