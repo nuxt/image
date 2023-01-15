9e2183f7d3a2c3de8fc76f500f58b69f6587d4a5
Daniel, Petrica Andrei-Daniel <andrei@danielpetrica.com>
2023-01-16 00:30:30 +0100

    fix: Edit unsplash provider to support image urls returned by the api by correctly appending the format query params.
    I found out that when using URLs which already contains query parameters, the provider will wrongly attach a '?' Before the applied operations and Unsplash would ignore the first passed operation.
    In these cases, placing a & would be a better option.
    I think another workaround would be to always place a & after ? like this: ?&
    Here's an example of one such link.

--- src/runtime/providers/unsplash.ts
+++ src/runtime/providers/unsplash.ts
@@ -8,7 +8,8 @@ const unsplashCDN = 'https://images.unsplash.com/'
 
 export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL = unsplashCDN } = {}) => {
   const operations = operationsGenerator(modifiers)
+  const hasQueryParams = src.includes('?')
   return {
-    url: withBase(joinURL(src + (operations ? ('?' + operations) : '')), baseURL)
+    url: withBase(joinURL(src + (operations ? ((hasQueryParams ? '&' : '?') + operations) : '')), baseURL)
   }
 }
