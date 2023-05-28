188d62973d2ac53024583ce3cff48734055b84e5
Daniel Roe <daniel@roe.dev>
2023-05-28 17:10:32 -0500

    chore: add docs to workspace and upgrade docus

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,14 +1,6 @@
-import { useNuxt } from 'nuxt/kit'
-
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   modules: ['@nuxtjs/plausible'],
-  hooks: {
-    'modules:before' () {
-      const nuxt = useNuxt()
-      nuxt.options.modules = nuxt.options.modules.filter(m => m !== 'nuxt-vitest')
-    }
-  },
   content: {
     documentDriven: {
       host: 'https://image.nuxtjs.org'
