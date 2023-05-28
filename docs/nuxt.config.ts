699e2ff25cb9d5ed648022cb341b2dbe9d9f6bad
Daniel Roe <daniel@roe.dev>
2023-05-28 16:58:30 -0500

    chore: remove nuxt-vitest module from docs site

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,14 @@
+import { useNuxt } from 'nuxt/kit'
+
 export default defineNuxtConfig({
   extends: '@nuxt-themes/docus',
   modules: ['@nuxtjs/plausible'],
+  hooks: {
+    'modules:before' () {
+      const nuxt = useNuxt()
+      nuxt.options.modules = nuxt.options.modules.filter(m => m !== 'nuxt-vitest')
+    }
+  },
   content: {
     documentDriven: {
       host: 'https://image.nuxtjs.org'
@@ -9,8 +17,4 @@ export default defineNuxtConfig({
   plausible: {
     domain: 'image.nuxtjs.org'
   },
-  // To by-pass .nuxtrc at the root
-  imports: {
-    autoImport: true
-  }
 })
