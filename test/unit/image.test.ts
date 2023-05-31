5c21f4cc1b35ce3e41825976eed854f1a4d882a6
Daniel Roe <daniel@roe.dev>
2023-05-31 22:35:46 +0100

    test: extract `mountImage` helper

--- test/unit/image.test.ts
+++ test/unit/image.test.ts
@@ -1,7 +1,7 @@
 // @vitest-environment nuxt
 
 import { beforeEach, describe, it, expect } from 'vitest'
-import { VueWrapper, mount } from '@vue/test-utils'
+import { ComponentMountingOptions, VueWrapper, mount } from '@vue/test-utils'
 
 import { NuxtImg } from '#components'
 
@@ -10,13 +10,11 @@ describe('Renders simple image', () => {
   const src = '/image.png'
 
   beforeEach(() => {
-    wrapper = mount(NuxtImg, {
-      propsData: {
-        width: 200,
-        height: 200,
-        sizes: '200,500:500,900:900',
-        src
-      }
+    wrapper = mountImage({
+      width: 200,
+      height: 200,
+      sizes: '200,500:500,900:900',
+      src
     })
   })
 
@@ -44,26 +42,24 @@ describe('Renders simple image', () => {
   })
 
   it('encodes characters', () => {
-    const img = mount(NuxtImg, {
-      propsData: {
-        width: 200,
-        height: 200,
-        sizes: '200,500:500,900:900',
-        src: '/汉字.png'
-      }
+    const img = mountImage({
+      width: 200,
+      height: 200,
+      sizes: '200,500:500,900:900',
+      src: '/汉字.png'
     })
     expect(img.html()).toMatchInlineSnapshot('"<img src=\\"/_ipx/s_900x900/%E6%B1%89%E5%AD%97.png\\" width=\\"200\\" height=\\"200\\" data-nuxt-img=\\"\\" sizes=\\"(max-width: 500px) 500px, 900px\\" srcset=\\"/_ipx/s_500x500/%E6%B1%89%E5%AD%97.png 500w, /_ipx/s_900x900/%E6%B1%89%E5%AD%97.png 900w\\">"')
   })
 
   it('correctly sets crop', () => {
-    const img = mount(NuxtImg, {
-      propsData: {
-        src: '/image.png',
-        width: 1000,
-        height: 2000,
-        sizes: 'xs:100vw sm:100vw md:300px lg:350px xl:350px 2xl:350px'
-      }
+    const img = mountImage({
+      src: '/image.png',
+      width: 1000,
+      height: 2000,
+      sizes: 'xs:100vw sm:100vw md:300px lg:350px xl:350px 2xl:350px'
     })
     expect(img.html()).toMatchInlineSnapshot('"<img src=\\"/_ipx/s_350x700/image.png\\" width=\\"1000\\" height=\\"2000\\" data-nuxt-img=\\"\\" sizes=\\"(max-width: 320px) 100vw, (max-width: 640px) 100vw, (max-width: 768px) 300px, (max-width: 1024px) 350px, (max-width: 1280px) 350px, 350px\\" srcset=\\"/_ipx/s_320x640/image.png 320w, /_ipx/s_640x1280/image.png 640w, /_ipx/s_300x600/image.png 300w, /_ipx/s_350x700/image.png 350w, /_ipx/s_350x700/image.png 350w, /_ipx/s_350x700/image.png 350w\\">"')
   })
 })
+
+const mountImage = (props: ComponentMountingOptions<typeof NuxtImg>['props']) => mount(NuxtImg, { props })
