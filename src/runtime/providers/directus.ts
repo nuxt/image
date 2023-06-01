328735931bd65824c47197d5bc0b8061dd6df99d
Daniel Roe <daniel@roe.dev>
2023-06-01 19:50:40 +0100

    refactor: remove defaults from operations generator

--- src/runtime/providers/directus.ts
+++ src/runtime/providers/directus.ts
@@ -3,36 +3,7 @@ import type { ProviderGetImage } from '../../types'
 import { createOperationsGenerator } from '#image'
 
 export const operationsGenerator = createOperationsGenerator({
-  keyMap: {
-    key: 'key',
-    width: 'width',
-    height: 'height',
-    format: 'format',
-    quality: 'quality',
-    fit: 'fit',
-    withoutEnlargement: 'withoutEnlargement',
-    transforms: 'transforms'
-  },
-  valueMap: {
-    fit: {
-      cover: 'cover',
-      contain: 'contain',
-      inside: 'inside',
-      outside: 'outside'
-    },
-    format: {
-      jpg: 'jpg',
-      png: 'png',
-      webp: 'webp',
-      tiff: 'tiff'
-    },
-    withoutEnlargement: {
-      true: 'true',
-      false: 'false'
-    }
-  },
-  joinWith: '&',
-  formatter: (key, value) => `${key}=${value}`
+  joinWith: '&'
 })
 
 export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL } = {}) => {
