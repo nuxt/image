f0326587b2fe9928489ef0415793c693c122a036
Marcel Pfeifer <marcel@wolves.ink>
2023-06-08 00:06:32 +0200

    style: linting

--- test/unit/picture.test.ts
+++ test/unit/picture.test.ts
@@ -1,157 +1,150 @@
 // @vitest-environment nuxt
 
-import { NuxtPicture } from "#components";
-import { VueWrapper, mount } from "@vue/test-utils";
-import { beforeEach, describe, expect, it } from "vitest";
+import { NuxtPicture } from '#components'
+import { VueWrapper, mount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it } from 'vitest'
 
-describe("Renders simple image", () => {
-  let wrapper: VueWrapper<any>;
-  const src = "/image.png";
+describe('Renders simple image', () => {
+  let wrapper: VueWrapper<any>
+  const src = '/image.png'
 
   const observer = {
     wasAdded: false,
-    wasDestroyed: false,
-  };
+    wasDestroyed: false
+  }
 
   beforeEach(() => {
     window.IntersectionObserver = class IntersectionObserver {
-      root: any;
-      rootMargin: any;
-      thresholds: any;
-      takeRecords: any;
+      root: any
+      rootMargin: any
+      thresholds: any
+      takeRecords: any
 
-      observe(_target: Element) {
-        observer.wasAdded = true;
+      observe (_target: Element) {
+        observer.wasAdded = true
       }
 
-      disconnect() {
-        observer.wasDestroyed = true;
+      disconnect () {
+        observer.wasDestroyed = true
       }
 
-      unobserve() {
-        observer.wasDestroyed = true;
+      unobserve () {
+        observer.wasDestroyed = true
       }
-    };
+    }
     wrapper = mount(NuxtPicture, {
       propsData: {
-        loading: "lazy",
+        loading: 'lazy',
         width: 200,
         height: 200,
-        sizes: "200,500:500,900:900",
-        src,
-      },
-    });
-  });
+        sizes: '200,500:500,900:900',
+        src
+      }
+    })
+  })
 
-  it("Matches snapshot", () => {
+  it('Matches snapshot', () => {
     expect(wrapper.html()).toMatchInlineSnapshot(`
       "<picture>
         <source type=\\"image/webp\\" sizes=\\"(max-width: 500px) 500px, 900px\\" srcset=\\"/_ipx/f_webp&s_500x500/image.png 500w, /_ipx/f_webp&s_900x900/image.png 900w\\"><img width=\\"200\\" height=\\"200\\" data-nuxt-pic=\\"\\" src=\\"/_ipx/f_png&s_900x900/image.png\\" sizes=\\"(max-width: 500px) 500px, 900px\\" srcset=\\"/_ipx/f_png&s_500x500/image.png 500w, /_ipx/f_png&s_900x900/image.png 900w\\">
       </picture>"
-    `);
-  });
+    `)
+  })
 
-  it.todo("alt attribute is generated");
+  it.todo('alt attribute is generated')
 
-  it("props.src is picked up by getImage()", () => {
-    [
-      ["source", "srcset", "/_ipx/f_webp&s_500x500/image.png"],
-      ["img", "src"],
-    ].forEach(([element, attribute, customSrc]) => {
-      const domSrc = wrapper.find(element).element.getAttribute(attribute);
-      expect(domSrc).toContain(customSrc || src);
-    });
-  });
+  it('props.src is picked up by getImage()', () => {
+    [['source', 'srcset', '/_ipx/f_webp&s_500x500/image.png'], ['img', 'src']].forEach(([element, attribute, customSrc]) => {
+      const domSrc = wrapper.find(element).element.getAttribute(attribute)
+      expect(domSrc).toContain(customSrc || src)
+    })
+  })
 
-  it("renders webp image source", () => {
-    expect(wrapper.find('[type="image/webp"]').exists()).toBe(true);
-  });
+  it('renders webp image source', () => {
+    expect(wrapper.find('[type="image/webp"]').exists()).toBe(true)
+  })
 
-  it("renders single format and fallback image tag", () => {
+  it('renders single format and fallback image tag', () => {
     const img = mount(NuxtPicture, {
       propsData: {
         width: 200,
         height: 200,
-        format: "avif",
-        src,
-      },
-    });
-    expect(img.find('source[type="image/avif"]').exists()).toBe(true);
-    expect(img.find("img").exists()).toBe(true);
-  });
+        format: 'avif',
+        src
+      }
+    })
+    expect(img.find('source[type="image/avif"]').exists()).toBe(true)
+    expect(img.find('img').exists()).toBe(true)
+  })
 
-  it("renders avif, webp and fallback image tag", () => {
+  it('renders avif, webp and fallback image tag', () => {
     const img = mount(NuxtPicture, {
       propsData: {
         width: 200,
         height: 200,
-        format: "avif,webp",
-        src,
-      },
-    });
-    expect(img.find('source[type="image/avif"]').exists()).toBe(true);
-    expect(img.find('source[type="image/webp"]').exists()).toBe(true);
-    expect(img.find("img").exists()).toBe(true);
-  });
+        format: 'avif,webp',
+        src
+      }
+    })
+    expect(img.find('source[type="image/avif"]').exists()).toBe(true)
+    expect(img.find('source[type="image/webp"]').exists()).toBe(true)
+    expect(img.find('img').exists()).toBe(true)
+  })
 
-  it("renders avif, gif and fallback image tag", () => {
+  it('renders avif, gif and fallback image tag', () => {
     const img = mount(NuxtPicture, {
       propsData: {
         width: 200,
         height: 200,
-        format: "avif,gif",
-        src,
-      },
-    });
-    expect(img.find('source[type="image/avif"]').exists()).toBe(true);
-    expect(img.find('source[type="image/gif"]').exists()).toBe(true);
-    expect(img.find("img").exists()).toBe(true);
-  });
+        format: 'avif,gif',
+        src
+      }
+    })
+    expect(img.find('source[type="image/avif"]').exists()).toBe(true)
+    expect(img.find('source[type="image/gif"]').exists()).toBe(true)
+    expect(img.find('img').exists()).toBe(true)
+  })
 
-  it("props.src is reactive", async () => {
-    const newSource = "/image.jpeg";
-    wrapper.setProps({ src: newSource });
+  it('props.src is reactive', async () => {
+    const newSource = '/image.jpeg'
+    wrapper.setProps({ src: newSource })
 
-    await nextTick();
-    [
-      ["source", "srcset", "/_ipx/f_webp&s_500x500/image.jpeg"],
-      ["img", "src"],
-    ].forEach(([element, attribute, src]) => {
-      const domSrc = wrapper.find(element).element.getAttribute(attribute);
-      expect(domSrc).toContain(src || newSource);
-    });
-  });
+    await nextTick()
 
-  it("sizes", () => {
-    const sizes = wrapper.find("source").element.getAttribute("sizes");
-    expect(sizes).toBe("(max-width: 500px) 500px, 900px");
-  });
+    ;[['source', 'srcset', '/_ipx/f_webp&s_500x500/image.jpeg'], ['img', 'src']].forEach(([element, attribute, src]) => {
+      const domSrc = wrapper.find(element).element.getAttribute(attribute)
+      expect(domSrc).toContain(src || newSource)
+    })
+  })
 
-  it("renders src when svg is passed", () => {
+  it('sizes', () => {
+    const sizes = wrapper.find('source').element.getAttribute('sizes')
+    expect(sizes).toBe('(max-width: 500px) 500px, 900px')
+  })
+
+  it('renders src when svg is passed', () => {
     const wrapper = mount(NuxtPicture, {
       propsData: {
-        src: "/image.svg",
-      },
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(
-      '"<picture><img data-nuxt-pic=\\"\\" src=\\"/image.svg\\"></picture>"'
-    );
-  });
+        src: '/image.svg'
+      }
+    })
+    expect(wrapper.html()).toMatchInlineSnapshot('"<picture><img data-nuxt-pic=\\"\\" src=\\"/image.svg\\"></picture>"')
+  })
 
-  it("encodes characters", () => {
+  it('encodes characters', () => {
     const img = mount(NuxtPicture, {
       propsData: {
-        loading: "lazy",
+        loading: 'lazy',
         width: 200,
         height: 200,
-        sizes: "200,500:500,900:900",
-        src: "/汉字.png",
-      },
-    });
+        sizes: '200,500:500,900:900',
+        src: '/汉字.png'
+      }
+    })
     expect(img.html()).toMatchInlineSnapshot(`
       "<picture>
         <source type=\\"image/webp\\" sizes=\\"(max-width: 500px) 500px, 900px\\" srcset=\\"/_ipx/f_webp&s_500x500/%E6%B1%89%E5%AD%97.png 500w, /_ipx/f_webp&s_900x900/%E6%B1%89%E5%AD%97.png 900w\\"><img width=\\"200\\" height=\\"200\\" data-nuxt-pic=\\"\\" src=\\"/_ipx/f_png&s_900x900/%E6%B1%89%E5%AD%97.png\\" sizes=\\"(max-width: 500px) 500px, 900px\\" srcset=\\"/_ipx/f_png&s_500x500/%E6%B1%89%E5%AD%97.png 500w, /_ipx/f_png&s_900x900/%E6%B1%89%E5%AD%97.png 900w\\">
       </picture>"
-    `);
-  });
-});
+    `)
+  })
+})
