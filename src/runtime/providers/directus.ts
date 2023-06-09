24c3d6f21b90121928e0223dc4411cee93de8651
Hartmut <info@hartmut.co.uk>
2023-06-09 20:28:18 +0200

    fix: for provider 'directus' deduplicate "transforms" modifier

--- src/runtime/providers/directus.ts
+++ src/runtime/providers/directus.ts
@@ -8,8 +8,10 @@ export const operationsGenerator = createOperationsGenerator({
 
 export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL } = {}) => {
   // Separating the transforms from the rest of the modifiers
-  const transforms = modifiers.transforms
+  let transforms = modifiers.transforms
   if (transforms && transforms.length > 0) {
+    // de-duplicate (can get multiplied when haveing >1 densities configured)
+    transforms = transforms.filter((value: any, index: any) => transforms.indexOf(value) === index)
     // We stringify and encode in URL the list of lists, then apply it back to the modifiers
     modifiers.transforms = new URLSearchParams(JSON.stringify(transforms)).toString().replace(/=+$/, '') as unknown as (string | number)[][]
   }
