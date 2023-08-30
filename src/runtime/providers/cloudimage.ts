c011599371999688696e5d9bb22d88e4c35ba843
Tibor <82872706+TiborPL@users.noreply.github.com>
2023-08-30 03:40:12 +0200

    feat!: set defaults to empty string, add warning

--- src/runtime/providers/cloudimage.ts
+++ src/runtime/providers/cloudimage.ts
@@ -21,18 +21,29 @@ const operationsGenerator = createOperationsGenerator({
   formatter: (key, value) => `${key}=${value}`
 })
 
+const isDev = process.env.NODE_ENV === 'development'
+
 // https://docs.cloudimage.io/go/cloudimage-documentation-v7/en/introduction
 export const getImage: ProviderGetImage = (src, {
   modifiers = {},
   baseURL = '',
-  token = 'demo',
-  apiVersion = 'v7',
+  token = '',
+  apiVersion = '',
   cdnURL = ''
 } = {}) => {
   const operations = operationsGenerator(modifiers)
   if (!cdnURL) {
     cdnURL = `https://${token}.cloudimg.io/${apiVersion}`
   }
+
+  if (isDev) {
+    // eslint-disable-next-line
+    console.warn('[cloudimage] <token> and <baseURL> has to be set to build image URL')
+    return {
+      url: joinURL('<token>', '<baseURL>', src) + (operations ? ('?' + operations) : '')
+    }
+  }
+
   return {
     url: joinURL(cdnURL, baseURL, src) + (operations ? ('?' + operations) : '')
   }
