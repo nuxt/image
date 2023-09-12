bd0075554be6a2b21a20cd6af9c4ae6249794aa7
Sébastien Chopin <seb@nuxt.com>
2023-09-12 17:42:54 +0200

    docs: remove unused styles

--- docs/app.vue
+++ docs/app.vue
@@ -75,21 +75,3 @@ provide('navigation', navigation)
     <LazyUDocsSearch :files="files" :navigation="navigation" :links="links" />
   </ClientOnly>
 </template>
-
-<style>
-html.dark {
-  color-scheme: dark;
-}
-
-.shiki {
-  padding: 0.6rem;
-  border-radius: 0.2rem;
-  border: 1px solid #8882;
-}
-
-html.dark .shiki,
-html.dark .shiki span {
-  color: var(--s-dark) !important;
-  background-color: var(--s-dark-bg) !important;
-}
-</style>
