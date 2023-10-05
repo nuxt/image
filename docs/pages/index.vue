cacdca423a9ba54885a646a5e3043b8cdc6b1f12
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2023-10-05 15:07:13 +0200

    docs: add title to provider links (#1029)
    Co-authored-by: Sébastien Chopin <seb@nuxt.com>

--- docs/pages/index.vue
+++ docs/pages/index.vue
@@ -89,7 +89,7 @@ const providers = ['cloudflare', 'cloudimage', 'cloudinary', 'directus', 'edgio'
       </UButton>
     </template>
     <div class="grid grid-cols-5 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-6 gap-4 sm:gap-5 lg:gap-6">
-      <NuxtLink v-for="(provider, index) in providers" :key="index" :to="`/providers/${provider}`" class="block lg:hover:scale-110 transition">
+      <NuxtLink v-for="(provider, index) in providers" :key="index" :to="`/providers/${provider}`" :title="provider" class="block lg:hover:scale-110 transition">
         <!-- <NuxtImg :placeholder="img(`/providers/${provider}.svg`, { h: 10, f: 'png', blur: 2, q: 50 })" :src="`/providers/${provider}.svg`" :alt="provider" width="64" height="64" class="w-12 h-12 sm:w-16 sm:h-16 rounded-xl" loading="lazy" /> -->
         <NuxtImg :src="`/providers/${provider}.svg`" :alt="provider" width="64" height="64" class="w-12 h-12 sm:w-16 sm:h-16 rounded-xl" loading="lazy" />
       </NuxtLink>
