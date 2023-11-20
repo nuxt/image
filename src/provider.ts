13a120ff35dc3a9d3dcfc0c116c0b81c32350840
Daniel Roe <daniel@roe.dev>
2023-11-20 13:59:16 +0000

    chore: add ignore

--- src/provider.ts
+++ src/provider.ts
@@ -74,6 +74,7 @@ export const providerSetup: Partial<Record<ImageProviderName, ProviderSetup>> = 
         },
         imageSettings: {
           sizes: Array.from(new Set(Object.values(moduleOptions.screens || {}))),
+          // @ts-ignore remove when updating to v3.8.2
           formats: ['image/jpeg', 'image/png', 'image/webp', 'image/avif'] satisfies NonNullable<NonNullable<NitroConfig['awsAmplify']>['imageSettings']>['formats'],
           minimumCacheTTL: 60 * 5,
           domains: moduleOptions.domains,
