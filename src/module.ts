b10cddbb6de91554b02298e4a968a6d3baae4abf
Damian Głowala <damian.glowala.rebkow@gmail.com>
2024-03-27 17:46:58 +0100

    fix: don't attempt to parse falsy values in `options.domains` (#1305)

--- src/module.ts
+++ src/module.ts
@@ -58,10 +58,9 @@ export default defineNuxtModule<ModuleOptions>({
     options.dir = resolve(nuxt.options.srcDir, options.dir)
 
     // Normalize domains to hostname
-    options.domains = options.domains.map((d) => {
-      if (!d.startsWith('http')) { d = 'http://' + d }
-      return parseURL(d).host
-    }).filter(Boolean) as string[]
+    options.domains = options.domains
+      .map(d => d && (parseURL(d.startsWith('http') ? d : ('http://' + d)).host))
+      .filter(Boolean) as string[]
 
     // Normalize alias to start with leading slash
     options.alias = Object.fromEntries(Object.entries(options.alias).map(e => [withLeadingSlash(e[0]), e[1]]))
