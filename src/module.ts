e2aa8fff87eb26ca642fb4b928290e903d4ef673
Damian Głowala <damian.glowala.rebkow@gmail.com>
2024-03-27 16:13:20 +0100

    fix: apply boolean filter to `options.domains` before mapping values

--- src/module.ts
+++ src/module.ts
@@ -58,10 +58,12 @@ export default defineNuxtModule<ModuleOptions>({
     options.dir = resolve(nuxt.options.srcDir, options.dir)
 
     // Normalize domains to hostname
-    options.domains = options.domains.map((d) => {
-      if (!d.startsWith('http')) { d = 'http://' + d }
-      return parseURL(d).host
-    }).filter(Boolean) as string[]
+    options.domains = (options.domains
+      .filter(Boolean) as string[])
+      .map((d) => {
+        if (!d.startsWith('http')) { d = 'http://' + d }
+        return parseURL(d).host
+      }) as string[]
 
     // Normalize alias to start with leading slash
     options.alias = Object.fromEntries(Object.entries(options.alias).map(e => [withLeadingSlash(e[0]), e[1]]))
