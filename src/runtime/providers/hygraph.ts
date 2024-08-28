411171607af61277fb026a02abdd414f9b2c7c89
Daniel Roe <daniel@roe.dev>
2024-08-28 15:53:16 +0100

    chore: revert hygraph changes

--- src/runtime/providers/hygraph.ts
+++ src/runtime/providers/hygraph.ts
@@ -1,4 +1,4 @@
-import { joinURL, withoutTrailingSlash } from 'ufo'
+import { joinURL } from 'ufo'
 import type { ProviderGetImage } from '@nuxt/image'
 
 type ImageOptimizations = {
@@ -9,7 +9,7 @@ type ImageOptimizations = {
   quality?: number
 }
 
-function getImageFormat(format?: string) {
+export function getImageFormat(format?: string) {
   let result = 'auto_image'
 
   if (format && format !== 'auto_image') {
@@ -19,24 +19,32 @@ function getImageFormat(format?: string) {
   return result
 }
 
-function splitUpURL(baseURL: string, url: string) {
-  /**
-   * https://eu-central-1-shared-euc1-02.graphassets.com/cltsj3mii0pvd07vwb5cyh1ig/cltsrex89477t08unlckqx9ue
-   *  - baseId: cltsj3mii0pvd07vwb5cyh1ig
-   *  - imageId: cltsrex89477t08unlckqx9ue
-   */
-  const escapedBaseURL = withoutTrailingSlash(baseURL).replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&')
-  const { groups } = url.match(new RegExp(`^(?:${escapedBaseURL}/)?(?<baseId>[^/]+)/(?<imageId>[^/]+)`)) || {}
+export function splitUpURL(url: string, baseURL: string) {
+  // Starting Image URL: https://eu-central-1-shared-euc1-02.graphassets.com/cltsj3mii0pvd07vwb5cyh1ig/cltsrex89477t08unlckqx9ue
 
-  if (!groups) {
-    throw new TypeError('[nuxt] [image] [hygraph] Invalid image URL')
+  // get Both IDs split off of the baseURL
+  // -> cltsj3mii0pvd07vwb5cyh1ig/cltsrex89477t08unlckqx9ue
+  const bothIds = url.split(`${baseURL}/`)[1]
+
+  // get baseId
+  // -> cltsj3mii0pvd07vwb5cyh1ig
+  // @ts-expect-error fixing in separate PR
+  const baseId = bothIds.split('/')[0]
+
+  // get imageId
+  // -> cltsrex89477t08unlckqx9ue
+  const imageId = url.split(`/`)[url.split(`/`).length - 1]
+
+  return {
+    baseId,
+    imageId,
   }
-
-  return groups as { baseId: string, imageId: string }
 }
 
-function optimizeHygraphImage(baseURL: string, url: string, optimizations: ImageOptimizations) {
-  const { baseId, imageId } = splitUpURL(baseURL, url)
+export function optimizeHygraphImage(baseURL: string, url: string, optimizations: ImageOptimizations) {
+  baseURL = baseURL.replace(/\/+$/, '')
+
+  const { baseId, imageId } = splitUpURL(url, baseURL)
   const imageFormat = getImageFormat(optimizations.format)
   const optimBase = 'resize'
   const quality = optimizations.quality && imageFormat !== 'auto_image' ? `quality=value:${optimizations.quality}/` : ''
@@ -54,13 +62,23 @@ function optimizeHygraphImage(baseURL: string, url: string, optimizations: Image
   }
 
   const optim = `${optimBase}=${optimList.join(',')}`
+  // @ts-expect-error fixing in separate PR
   const result = joinURL(baseURL, baseId, optim, quality, imageFormat, imageId)
 
   return result
 }
 
-export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL } = {}) => {
-  const { width, height, fit, format, quality } = modifiers
+export const getImage: ProviderGetImage = (
+  src,
+  { modifiers = {}, baseURL } = {},
+) => {
+  const {
+    width,
+    height,
+    fit,
+    format,
+    quality,
+  } = modifiers
 
   if (!baseURL) {
     throw new Error('No Hygraph image base URL provided.')
