60af72a2a7e6071d48547d187fb4e4be49b09b63
Daniel Roe <daniel@roe.dev>
2025-03-22 10:19:17 +0000

    chore: allow overriding event

--- src/runtime/composables.ts
+++ src/runtime/composables.ts
@@ -5,13 +5,13 @@ import { createImage } from './image'
 import { imageOptions } from '#build/image-options.mjs'
 import { useNuxtApp, useRuntimeConfig } from '#imports'
 
-export const useImage = (_event?: H3Event): $Img => {
+export const useImage = (event?: H3Event): $Img => {
   const config = useRuntimeConfig()
   const nuxtApp = useNuxtApp()
 
   return nuxtApp.$img as $Img || nuxtApp._img || (nuxtApp._img = createImage({
     ...imageOptions,
-    event: nuxtApp.ssrContext?.event,
+    event: event || nuxtApp.ssrContext?.event,
     nuxt: {
       baseURL: config.app.baseURL,
     },
