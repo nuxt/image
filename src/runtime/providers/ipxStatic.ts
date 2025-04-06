2a01a7eba69ef0b89a969b8a55f4eaef18861e75
Daniel Roe <daniel@roe.dev>
2025-04-06 07:22:01 -0700

    fix(ipxStatic): strip repeated slashes from image path (#1801)

--- src/runtime/providers/ipxStatic.ts
+++ src/runtime/providers/ipxStatic.ts
@@ -1,1 +1,23 @@
-export * from './ipx'
+import { joinURL, encodePath } from 'ufo'
+import type { ProviderGetImage } from '../../module'
+import { operationsGenerator } from './ipx'
+
+export const getImage: ProviderGetImage = (src, { modifiers = {}, baseURL } = {}, ctx) => {
+  if (modifiers.width && modifiers.height) {
+    modifiers.resize = `${modifiers.width}x${modifiers.height}`
+    delete modifiers.width
+    delete modifiers.height
+  }
+
+  const params = operationsGenerator(modifiers) || '_'
+
+  if (!baseURL) {
+    baseURL = joinURL(ctx.options.nuxt.baseURL, '/_ipx')
+  }
+
+  return {
+    url: joinURL(baseURL, params, encodePath(src).replace(/\/{2,}/g, '/')),
+  }
+}
+
+export { validateDomains, supportsAlias } from './ipx'
