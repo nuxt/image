0e5d7081eed0ee93d78a39c031d2ad2b84b1bea2
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-06-05 11:12:03 +0200

    up

--- docus/app/components/AppHeaderCenter.vue
+++ docus/app/components/AppHeaderCenter.vue
@@ -1,3 +1,20 @@
+<script setup lang="ts">
+const links = [{
+  label: 'Documentation',
+  icon: 'i-lucide-book-open',
+  to: '/get-started/installation',
+}, {
+  label: 'Playground',
+  icon: 'i-lucide-play',
+  to: '/playground',
+}, {
+  label: 'Releases',
+  icon: 'i-lucide-rocket',
+  to: 'https://github.com/nuxt/image/releases',
+  target: '_blank',
+}]
+</script>
+
 <template>
-  <div />
+  <UNavigationMenu :items="links" />
 </template>
