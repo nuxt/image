5610fe0f0e84d4442565c7465adfb1cd774058e8
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-06-17 12:29:52 +0200

    chore(deps): upgrade docus to fix meta template title (#1869)

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,9 +1,4 @@
 export default defineNuxtConfig({
-  app: {
-    head: {
-      titleTemplate: '%s - Nuxt Image',
-    },
-  },
   css: ['../app/assets/css/main.css'],
   site: {
     name: 'Nuxt Image',
