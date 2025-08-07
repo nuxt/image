48bed0a7e70f34b34abb67241286e33b8138ae14
Baptiste Leproux <leproux.baptiste@gmail.com>
2025-08-07 17:12:25 +0200

    docs(deps): upgrade to docus v4

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,16 +1,11 @@
 export default defineNuxtConfig({
-
+  extends: ['docus'],
   modules: ['@nuxtjs/plausible'],
   css: ['../app/assets/css/main.css'],
-
   site: {
     name: 'Nuxt Image',
   },
-
-  future: {
-    compatibilityVersion: 4,
-  },
-
+  compatibilityDate: '2025-08-07',
   llms: {
     domain: 'https://image.nuxt.com',
     description: 'Nuxt Image is a module for Nuxt to optimize image for best performance.',
