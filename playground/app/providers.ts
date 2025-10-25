aee9ffff291db2d576ac9f163f872ffb6dc29de2
Luke Nelson <luke@nelson.zone>
2025-10-25 21:35:14 +0100

    feat: add github provider to playground

--- playground/app/providers.ts
+++ playground/app/providers.ts
@@ -321,6 +321,24 @@ export const providers: Provider[] = [
       },
     ],
   },
+  // GitHub
+  {
+    name: 'github',
+    samples: [
+      {
+        src: 'npm',
+        width: 50,
+        height: 50,
+      },
+      {
+        src: 'nuxt',
+        width: 50,
+        height: 100,
+      }, {
+        src: 'unjs',
+      },
+    ],
+  },
   // gumlet
   {
     name: 'gumlet',
