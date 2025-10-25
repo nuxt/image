8e30a510f2016696ed355b808233ae303630a3f1
Luke Nelson <luke@nelson.zone>
2025-10-25 21:50:05 +0100

    fix type error for baseURL

--- src/runtime/providers/github.ts
+++ src/runtime/providers/github.ts
@@ -1,6 +1,5 @@
 import { encodeQueryItem, joinURL } from 'ufo'
 import { createOperationsGenerator } from '../utils/index'
-import type { ProviderGetImage } from '@nuxt/image'
 import { defineProvider } from '../utils/provider'
 
 const operationsGenerator = createOperationsGenerator({
@@ -8,21 +7,25 @@ const operationsGenerator = createOperationsGenerator({
   formatter: (key, value) => encodeQueryItem(key, value),
 })
 
-export const getImage: ProviderGetImage = (src, { modifiers, baseURL = 'https://avatars.githubusercontent.com/' }) => {
-  let size = 460 // Default size
-  // Calculate size based on width/height
-  if (modifiers?.width || modifiers?.height) {
-    size = Math.max(modifiers?.height ?? 0, modifiers?.width ?? 0)
-  }
-
-  const operations = operationsGenerator({
-    v: 4,
-    s: size,
-  })
-
-  return {
-    url: joinURL(baseURL, src + '?' + operations),
-  }
+interface GitHubOptions {
+  baseURL: string
 }
 
-export default defineProvider({ getImage })
+export default defineProvider<GitHubOptions>({
+  getImage: (src, { modifiers, baseURL = 'https://avatars.githubusercontent.com/' }) => {
+    let size = 460 // Default size
+    // Calculate size based on width/height
+    if (modifiers?.width || modifiers?.height) {
+      size = Math.max(modifiers?.height ?? 0, modifiers?.width ?? 0)
+    }
+
+    const operations = operationsGenerator({
+      v: 4,
+      s: size,
+    })
+
+    return {
+      url: joinURL(baseURL, src + '?' + operations),
+    }
+  },
+})
